d64626a46b2bced0993ff215514221f49a0ac0ff
hyuliasadula <huliya1401@gmail.com>
2024-08-12 08:19:18 +0300

    last exercises for loops

---
+++ 05.WhileLoop/15.Clock/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _15.Clock
+{
+    internal class Program
+    {
+        /*
+         Напишете програма, която отпечатва часовете в денонощието от 0:00 до 23:59, всеки на отделен ред. Часовете трябва да се изписват във формат "{час}:{минути}".
+         */
+        static void Main(string[] args)
+        {
+            int hours = 23;
+            int minutes = 59;
+
+            for (int h = 0; h <= hours; h++)
+            {
+                for (int m = 0; m <= minutes ; m++)
+                {
+                    if (m <10)
+                    {
+                        Console.WriteLine($"{h}:0{m}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{h}:{m}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 05.WhileLoop/16.MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _16.MultiplicationTable
+{
+    internal class Program
+    {
+        /*
+         Отпечатайте на конзолата таблицата за умножение за числата от 1 до 10 във формат: 
+        "{първи множител} * {втори множител} = {резултат}". 
+
+         */
+        static void Main(string[] args)
+        {
+            int firstNum = 10;
+            int secondNum = 10;
+
+            for (int first = 1; first <= firstNum ; first++)
+            {
+                for (int second = 1; second <= secondNum ; second++)
+                {
+                    Console.WriteLine($"{first} * {second} = {first * second}");
+                }
+            }
+        }
+    }
+}
