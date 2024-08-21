2c47bc239e6a7da0af688e95d996ecafefb38e9e
hyuliasadula <huliya1401@gmail.com>
2024-08-21 12:55:05 +0300

    task 8 and 9 done

---
+++ 08.Lists/08.Train/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Train
+{
+    internal class Program
+    {
+        /*
+         On the first line, we will receive a list of wagons (integers). Each integer represents the number of passengers that are currently in each wagon of the passenger train. On the next line, you will receive the max capacity of a wagon, represented as a single integer. Until you receive the "end" command, you will be receiving two types of input:
+
+            Add {passengers} – add a wagon to the end of the train with the given number of passengers
+            {passengers} – find a single wagon to fit all the incoming passengers (starting from the first wagon).
+            In the end, print the final state of the train (all the wagons separated by a space).
+
+
+                Input                            Output
+                                            72 54 21 12 4 75 23 10 0
+                32 54 21 12 4 0 23                      
+                75
+                Add 10
+                Add 0
+                30
+                10
+                75
+                end 
+         
+         */
+        static void Main(string[] args)
+        {
+            List<int> passengersPerWagon = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int maxWagonCapacity = int.Parse(Console.ReadLine());
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                List<string> command = input.Split(" ").ToList();
+                if (command[0] == "Add")
+                {
+                    int strToNumber = int.Parse(command[1]);
+                    passengersPerWagon.Add(strToNumber);
+                
+                }
+                else
+                {
+                    int passengers = int.Parse(command[0]);
+                    for (int y = 0; y < passengersPerWagon.Count; y++)
+                    {
+                        if (passengersPerWagon[y] + passengers <= maxWagonCapacity)
+                        {
+                            passengersPerWagon[y] += passengers;
+                            break; 
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", passengersPerWagon));
+
+        }
+    }
+}
---
+++ 08.Lists/09.ChangeList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ChangeList
+{
+    internal class Program
+    {
+        /*
+         Create a program, that reads a list of integers from the console and receives commands to manipulate the list.
+
+            Your program may receive the following commands:
+            Delete {element} – delete all elements in the array, which are equal to the given element.
+            Insert {element} {position} – insert the element at the given position.
+            You should exit the program when you receive the "end" command. Print all numbers in the array, separated by a single whitespace.
+
+                    Input                               Output
+
+                    1 2 3 4 5 5 5 6                 1 10 2 3 4 6
+                    Delete 5 
+                    Insert 10 1
+                    Delete 5
+                    end
+         
+         */
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                List<string> command = input.Split(" ").ToList();
+                if (command[0] == "Delete")
+                {
+                    int elementToRemove = int.Parse(command[1]);
+                    // RemoveAll(x => x == elementToRemove)
+                    while (list.Contains(elementToRemove))
+                    {
+                        list.Remove(elementToRemove);
+                    }
+                }
+                else if (command[0] == "Insert")
+                {
+                    int element = int.Parse(command[1]);
+                    int position = int.Parse(command[2]);
+                    list.Insert(position, element);
+                }
+            }
+            Console.WriteLine(string.Join(" ",list));
+        }
+    }
+}
